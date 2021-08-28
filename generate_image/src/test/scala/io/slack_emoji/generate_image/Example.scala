40a438ef077b1d56cf576a5f1bfd46a3c43d7aef
4afS <4afs.h1@gmail.com>
2021-08-29 04:45:04 +0900

    add test name

--- generate_image/src/test/scala/io/slack_emoji/generate_image/Example.scala
+++ generate_image/src/test/scala/io/slack_emoji/generate_image/Example.scala
@@ -4,4 +4,9 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class Example extends AnyWordSpec with Matchers:
-  1 + 1 shouldEqual 2
+
+  "example" should {
+    "test" in {
+      1 + 1 shouldEqual 2
+    }
+  }
